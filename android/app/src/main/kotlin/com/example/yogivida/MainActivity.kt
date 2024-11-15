d92ef0a5cd69ef1cfc37db3514d0d7ca1b8be707
yoricky42 <yorickyves42@gmail.com>
2024-11-15 09:23:10 +0000

    save connectivity

--- android/app/src/main/kotlin/com/example/yogivida/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/yogivida/MainActivity.kt
@@ -1,5 +1,9 @@
-package com.guindyprojects.yogivida
-
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugins.connectivityplus.ConnectivityPlusPlugin
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine() {
+        super.configureFlutterEngine()
+        ConnectivityPlusPlugin.registerWith(flutterEngine?.dartExecutor)
+    }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,24 @@
-import 'package:authentication_repository/authentication_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:authentication_repository/authentication_repository.dart';
 import 'package:user_repository/user_repository.dart';
-import 'package:yogivida_mobile/constant.dart';
-import 'package:yogivida_mobile/screens/Boutique/Boutique.dart';
-import 'package:yogivida_mobile/screens/Compte/MonCompte.dart';
-import 'package:yogivida_mobile/screens/Home/MainHome.dart';
-import 'package:yogivida_mobile/screens/auth/login_screen.dart';
-import 'package:yogivida_mobile/screens/auth/register_screen.dart';
-import 'package:yogivida_mobile/screens/Home/home_page.dart';
-import 'package:yogivida_mobile/screens/splash/splash_screen.dart';
-import 'package:intl/date_symbol_data_local.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:yogivida_mobile/components/ConnectionNotifier.dart';
+import 'package:yogivida_mobile/services/connection/Connectivity_service.dart'; // Le service de connectivité
+import 'package:yogivida_mobile/components/ConnectionNotifier.dart'; // Le ConnectionNotifier
 import 'package:yogivida_mobile/services/authBloc/auth_bloc_bloc.dart';
 import 'package:yogivida_mobile/services/authentication_bloc/authentication_bloc.dart';
+import 'package:yogivida_mobile/constant.dart';
+import 'package:yogivida_mobile/screens/Home/MainHome.dart';
+import 'package:yogivida_mobile/screens/auth/login_screen.dart';
+import 'package:yogivida_mobile/screens/Home/home_page.dart';
 import 'core/models/user_model.dart';
 import 'firebase_options.dart';
-// import 'screens/splash/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 Future<void> main() async {
-  // S'assurer que Flutter est initialisé avant d'appeler initializeDateFormatting
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Initialiser la locale pour le formatage des dates
   await initializeDateFormatting('fr_FR', null);
-
-  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -46,10 +39,14 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    _userRepository = UserRepository<Utilisateur>(factoryFunction: (json) => Utilisateur.fromJson(json));
-    _authenticationRepository = AuthenticationRepository(loginUrl: "$BASE_URL$LOGIN_ENDPOINT", registrationUrl: "$BASE_URL$LOGIN_ENDPOINT", logoutUrl: "$BASE_URL$LOGIN_ENDPOINT", userRepository: _userRepository);
+    _userRepository = UserRepository<Utilisateur>(
+        factoryFunction: (json) => Utilisateur.fromJson(json));
+    _authenticationRepository = AuthenticationRepository(
+        loginUrl: "$BASE_URL$LOGIN_ENDPOINT",
+        registrationUrl: "$BASE_URL$LOGIN_ENDPOINT",
+        logoutUrl: "$BASE_URL$LOGIN_ENDPOINT",
+        userRepository: _userRepository);
   }
-
 
   @override
   void dispose() {
@@ -58,62 +55,42 @@ class _MyAppState extends State<MyApp> {
     super.dispose();
   }
 
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MultiRepositoryProvider(
-        providers:  [
-          RepositoryProvider<AuthenticationRepository>(
-            create: (context) => _authenticationRepository,
-          ),
-          RepositoryProvider<UserRepository<Utilisateur>>(
-            create: (context) => _userRepository,
+      providers: [
+        RepositoryProvider<AuthenticationRepository>(
+          create: (context) => _authenticationRepository,
+        ),
+        RepositoryProvider<UserRepository<Utilisateur>>(
+          create: (context) => _userRepository,
+        ),
+      ],
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => AuthenticationBloc<Utilisateur>(
+              authenticationRepository: _authenticationRepository,
+              userRepository: _userRepository,
+            ),
           ),
         ],
-        child: MultiBlocProvider(
-          providers: [
-            BlocProvider(
-              create: (context) => AuthenticationBloc<Utilisateur>(
-                authenticationRepository: _authenticationRepository,
-                userRepository: _userRepository,
-              ),
-              // create: (context) => AuthenticationBloc<Utilisateur>()..add(AppStartedEvent(data: null)),
-
-            ),
-          ],
+        child: ConnectionNotifier(
+          // Intégrer le notifier ici
           child: MaterialApp(
             title: 'Flutter Demo',
             theme: ThemeData(
               primarySwatch: Colors.blueGrey,
-              // primarySwatch: Color(0xff15274d),
             ),
             initialRoute: '/',
             routes: {
-              // '/': (context) => RegisterScreen(),
               '/': (context) => const Mainhome(),
               '/login': (context) => const LoginScreen(),
               '/home': (context) => const HomePage(),
             },
-          )
+          ),
         ),
+      ),
     );
-    //   BlocProvider(
-    //   create: (context) => AuthenticationBloc<Utilisateur>()..add(AppStartedEvent(data: null)),
-    //   child: MaterialApp(
-    //     title: 'Flutter Demo',
-    //     theme: ThemeData(
-    //       primarySwatch: Colors.blueGrey,
-    //       // primarySwatch: Color(0xff15274d),
-    //     ),
-    //     initialRoute: '/',
-    //     routes: {
-    //       // '/': (context) => RegisterScreen(),
-    //       '/': (context) => SplashScreen(),
-    //       '/login': (context) => const LoginScreen(),
-    //       '/home': (context) => const HomePage(),
-    //     },
-    //   ),
-    // );
   }
 }
