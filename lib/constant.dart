07407c708e0585c92b452682a58b4d53ed782b0e
Sakhewar Diop <sakhewardiop@guindytechnology.com>
2025-01-03 09:01:57 +0000

    update

--- lib/constant.dart
+++ lib/constant.dart
@@ -5,7 +5,7 @@ const REGISTRATION_ENDPOINT = "inscription";
 const LOGIN_ENDPOINT = "connexion";
 const LOGOUT_ENDPOINT = "deconnexion";
 const REQUEST_PWD_ENDPOINT = "password-reset";
-const BASE_URL = 'https://7967-41-85-178-219.ngrok-free.app/yogivida_back/public/';
+const BASE_URL = 'http://192.168.1.12:8001/yogivida_back/public/';
 // const BASE_URL = 'https://yogi-vida.com/yogivida_back_test/';
 const BASE_URL_QGL = '${BASE_URL}graphql?query=';
 
