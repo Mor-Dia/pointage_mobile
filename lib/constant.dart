8539381e7b9960d56810f0f2e1c462d85caa915d
Guindy_thiam <thiam@guindytechnology.com>
2025-11-03 14:58:55 +0000

    a jour

--- lib/constant.dart
+++ lib/constant.dart
@@ -9,10 +9,8 @@ const LOGIN_ENDPOINT = "connexion";
 const LOGOUT_ENDPOINT = "deconnexion";
 // const REQUEST_PWD_ENDPOINT = "password-reset";
 const REQUEST_PWD_ENDPOINT = "password-create";
-// const BASE_URL = 'https://yogi-vida.com/yogivida_back/';
 const BASE_URL = 'https://yogi-vida.com/yogivida_back/';
 // const BASE_URL = 'https://yogi-vida.com/yogivida_back_test/';
-// const BASE_URL = 'http://localhost/yogivida_back/public/';
 const BASE_URL_QGL = '${BASE_URL}graphql?query=';
 
 const Color primaryColor = Color(0xff15274D);
